2ba9a81f1c26da6dccd2458e473d49c0da3b1ffd
GOlivierNation <ogasominali@gmail.com>
2022-08-31 16:49:50 -0400

    Fix Linters Errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Comment
 class ApplicationController < ActionController::Base
   def current_user
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Comment
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
