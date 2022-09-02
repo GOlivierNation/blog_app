be3855f8f1f58ccb0ab601f2aed6960cf52b5044
GOlivierNation <ogasominali@gmail.com>
2022-09-02 09:14:36 -0400

    Fix Linters error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Comment
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Comment
 class CommentsController < ApplicationController
   def new
